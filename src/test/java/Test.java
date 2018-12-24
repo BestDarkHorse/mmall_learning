905a1e4a3be0ec4d8dd92d5a3f69e275c5933453
imooc <sundongkui67@163.com>
2018-12-25 06:21:24 +0800

    支付模块

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,7 @@
+import com.mmall.util.BigDecimalUtil;
 import com.mmall.util.DateTimeUtil;
 
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class Test {
@@ -11,6 +13,15 @@ public class Test {
 
         Date date = DateTimeUtil.strToDate("2018-10-20 9:51:09");
         System.out.println("字符串转日期格式 "+date.toString());
+    }
+
+    @org.junit.Test
+    public void testBigDecimal() {
+        Integer a = 100;
+        Integer b = 3;
+        System.out.println("Integer 除法"+a/b);
+        BigDecimal div = BigDecimalUtil.div(a.doubleValue(), b.doubleValue());
+        System.out.println("BigDecimal 除法" + div);
     }
 
 }
