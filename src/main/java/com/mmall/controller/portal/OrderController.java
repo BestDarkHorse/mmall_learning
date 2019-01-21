a39866ec1582dfee9f61d9494ace2423b81c5d10
dongkui.sun <dongkui.sun@xinboxinmo.com>
2019-01-21 19:28:21 +0800

    测试回滚行

--- src/main/java/com/mmall/controller/portal/OrderController.java
+++ src/main/java/com/mmall/controller/portal/OrderController.java
@@ -138,7 +138,6 @@ public class OrderController {
         }
 
         //todo 驗證各種數據
-        //todo 继续验证数据
 
         ServerResponse serverResponse = iOrderService.aliCallback(params);
         if (serverResponse.isSuccess()) {
